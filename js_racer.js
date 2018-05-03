2d88b3f032b89764113f30fefbcc1f7bdf7a26eb
Rhesa Utomo <venderblitz@gmail.com>
2018-05-03 17:40:04 +0700

    refactor code, does not include user input yet

--- js_racer.js
+++ js_racer.js
@@ -1,6 +1,19 @@
+// let argv = process.argv;
+// playerSize = argv[2];
+// trackSize = argv[3];
+
 const players = ['a', 'b', 'c'];
 const trackSize = 10;
 const playerPos = [0, 0, 0];
+
+function sleep(milliseconds) {
+  let start = new Date().getTime();
+  for (let i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
+}
 
 function dice () {
   const max = 6;
@@ -10,7 +23,7 @@ function dice () {
 
 function printLine (player, pos) {
   const line = [];
-  for (let i = 0; i < trackSize; i++) {
+  for (let i = 0; i <= trackSize; i++) {
     if (pos === i) {
       line.push(player);
     } else {
@@ -20,28 +33,51 @@ function printLine (player, pos) {
   console.log(line.join('|'));
 }
 
-function printBoard (player) {
-  const charIndex = players.indexOf(player);
-  printLine(player, playerPos[charIndex]);
+function printBoard () {
+  console.log(playerPos);
+  for (let i = 0; i < players.length; i++) {
+    printLine(players[i], playerPos[i]);
+  }
 }
 
 function advancedPlayer (player) {
   const charIndex = players.indexOf(player);
-  playerPos[charIndex] += dice();
+  const diceRoll = dice();
+  if ((playerPos[charIndex] + diceRoll) > trackSize) {
+    playerPos[charIndex] = trackSize;
+    return true;
+  } else {
+    playerPos[charIndex] += diceRoll;
+    return false;
+  }
 }
 
 function finished () {
-  
+  return playerPos.indexOf(trackSize) !== -1
 }
 
-while (!finished()) {
+function winner () {
+  const winnerIndex = playerPos.indexOf(trackSize);
+  console.log('Your winner: ', players[winnerIndex]);
+}
+
+function resetBoard() {
+  console.log("\x1B[2J")
+}
+
+let done = false;
+
+while (!done) {
   for (let i = 0; i < players.length; i++) {
     advancedPlayer(players[i]);
-    printBoard(players[i]);
-    // sleep(500);
-    // resetBoard();
+    printBoard();
+    done = finished();
+    if (done) {
+      winner();
+      break;
+    }
+    sleep(500);
   }
-
 }
 
 
