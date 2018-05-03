cf5dccaec29ef856ef665722c73b101f07919605
Tushar <tusharbedi7373@hotmail.com>
2018-05-03 22:11:53 +0700

    INCOMPLETE js racer project

--- js_racer.js
+++ js_racer.js
@@ -1,7 +1,24 @@
-"use strict"
+// function createBoard (numOfPlayers,boardLength){
+//   const board =[]
+//
+//   // insert number of players here
+//   let players = getPlayers()  // if 3 => [a,b,c]
+//
+//   for (let i = 0; i < players.length; i++) {
+//     print_line(players[i], 0, board)
+//     // let nestedArr = []
+//     // for (let j = 0; j < boardLength+1; j++) {
+//     //    nestedArr.push('')
+//     // }
+//     // board.push(nestedArr.join(" | "))
+//     // console.log(arr)
+//   }
+//   console.log(board);
+// }
 
 function dice(){
-
+  let roll = Math.ceil(Math.random()*5 +1);
+  return roll
 }
 
 function sleep(milliseconds) {
@@ -13,17 +30,46 @@ function sleep(milliseconds) {
   }
 }
 
-function print_board() {
-
+function getPlayers ( numOfPlayers ) {
+  let nameChoice = 'abcdefghijklmnopqrstuvwxyz';
+  let counter = 0
+  let outputPlayers = []
+  for (let a = 0; a < numOfPlayers; a++) { //num = 3
+    outputPlayers.push (nameChoice[a])
+  }
+  return outputPlayers
 }
 
-function print_line(player, pos) {
-
+function create_line (boardLength) {
+  let board = [];
+  let players = getPlayers (3) //insert number of players
+  let currentPosition = playerPosition (3)
+  // let players = ['a','b','c']
+  for (let i = 0; i < players.length; i++) {
+    let line = [];
+      for (let j = 0; j < boardLength; j++) {
+      if(j == 0) {
+        line.push(players[i])
+      } else {
+        line.push(' ');
+      }
+    }
+    board.push(line.join('|'))
+  }
+  console.log(board)
 }
 
-function advanced_player(player) {
+// create_line(15)
 
+function playerPosition (numOfPlayers) {
+  let eachPosition = []
+  for (let i = 0; i < numOfPlayers; i++) {
+    eachPosition.push(0)
+  }
+  return eachPosition
 }
+
+// playerPosition();
 
 function finished() {
 
@@ -34,3 +80,6 @@ function winner() {
 function reset_board() {
   console.log("\x1B[2J")
 }
+
+//ddriver code
+let playingBoard = createBoard(3,15)
