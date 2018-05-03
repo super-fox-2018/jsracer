bd9f9af75b6ab0a84057284ac6193cfbc86ebe5f
Rin Ng <Rin08.ng@gmail.com>
2018-05-03 15:11:01 +0700

    Added code to the js_racer.js file

--- js_racer.js
+++ js_racer.js
@@ -1,7 +1,8 @@
 "use strict"
 
 function dice(){
-
+  var dice = Math.floor(Math.random() * 6 + 1);
+  return dice;
 }
 
 function sleep(milliseconds) {
@@ -13,24 +14,126 @@ function sleep(milliseconds) {
   }
 }
 
-function print_board() {
+function print_board(player, line) {
+  var playerArr = advanced_player(player);
+  var arr = [];
+  var temp = "";
+  var origin = 0;
+  //Initial
+  for(var i = 0; i < playerArr.length; i++){
+    var lineArr = print_line(playerArr[i], 0, line);  //'a', 0, 15
+    arr.push(lineArr);
+  }
+
+  console.log(arr);
+  var position = [0,0,0];
+  var winner = [];
+  var maxIdx = line - 1;
+  do{
+    for(var j = 0; j < player; j++){
+      /* --------------------------------------
+                      FUNCTION METHOD
+      -----------------------------------------*/
+      var move = dice();
+      console.log("move: " + move)
+      if((position[j] + move) >maxIdx){
+        arr[j] = print_line(playerArr[j], maxIdx, line);
+        winner.push(playerArr[j]);
+      }
+      else{
+        arr[j] = print_line(playerArr[j], position[j] + move, line);
+        position[j] += move;
+      }
+      /* --------------------------------------
+                      SWAP METHOD
+      -----------------------------------------*/
+      /*var maxIdx = line -1;
+      var rowIdx = arr[j];
+
+      console.log("move: " + move);
+      if((position[j] + move) > maxIdx){
+        temp = rowIdx[position[j]];
+        rowIdx[position[j]] = rowIdx[maxIdx];
+        rowIdx[maxIdx] = temp;
+        winner.push(temp);
+
+      }
+      else{
+        temp = rowIdx[position[j]];
+        rowIdx[position[j]] = rowIdx[position[j]+move];
+        rowIdx[position[j] + move] = temp;
+        position[j] += move;
+      }*/
+
+      for(let i=0; i<arr.length; i++) {
+        console.log(arr[i].join('|'));
+      }
+    }
+  reset_board();
+ }while(finished(arr) !== true)
+
+
+  console.log(winner[0].toString().toUpperCase() + " IS THE WINNER!");
 
 }
 
-function print_line(player, pos) {
+function print_line(playerName, posPlayer, line) {  //'a', 0, 15
+  var lineArr = [];
+
+  if(line < 15){
+    return "Invalid line length"
+  }
+  else{
+    for(var k = 0; k < line; k++ ){
+      if(k !== posPlayer){
+        lineArr.push(" ");
+      }
+      else{
+        lineArr.push(playerName);
+      }
+    }
+    return lineArr;
+  }
 
 }
 
 function advanced_player(player) {
+  var str = "abcdefghijklmnopqrstuvxyz";
+  var arr = str.split("");
+  var playerArr = [];
+  if(player < 2 && player > 25){
+    return "Invalid player number"
+  }
+  else{
+    for(var i = 0; i < player;i++){
+      playerArr[i] = str[i];
+    }
+    return playerArr;
+  }
 
 }
 
-function finished() {
+function finished(arr) {
+  var maxIdx = arr[0].length - 1 ;
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i][maxIdx] !== " "){
+      return true;
+    }
+  }
+
+  return false;
 
 }
-function winner() {
+function winner(player) {
 
 }
 function reset_board() {
   console.log("\x1B[2J")
 }
+
+
+let argv = process.argv
+let player = argv[2];
+let line = argv[3];
+
+console.log(print_board(player, line));
