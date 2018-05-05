6452080df6dd38d59fd0533fafdc370ffaf36f18
David Yunus <dove.fit@gmail.comgit config --global user.name>
2018-05-05 12:27:20 +0700

    davidyunus

--- js_racer.js
+++ js_racer.js
@@ -1,7 +1,7 @@
 "use strict"
 
 function dice(){
-
+return Math.ceil(Math.random() * 6);
 }
 
 function sleep(milliseconds) {
@@ -13,24 +13,49 @@ function sleep(milliseconds) {
   }
 }
 
-function print_board() {
+function generatePlayers(player) {
+    let namePlayer = 'abcdefghijklmnopqrstuvwxyz';
+    let listPlayer = [];
 
+    for(let i = 0; i < player; i++) {
+        listPlayer.push(namePlayer[i]);
+    }
+    return listPlayer;
 }
 
-function print_line(player, pos) {
+function print_board(num, long) {
+    
+}
 
+function print_line(pos) {
+    let track = [];
+    
+    for(let i = 0; i < pos; i++) {
+        track.push(' ');
+    }
+   return track;
 }
 
 function advanced_player(player) {
+//untuk maju
 
 }
 
 function finished() {
 
 }
-function winner() {
 
+function winner() {
+    return 'Player ' + 'A' + 'win';
 }
 function reset_board() {
   console.log("\x1B[2J")
 }
+
+// console.log(dice());
+// while(!finished()){
+//     let players = generatePlayers(3)
+//     print_board(3,15)
+// }
+// console.log(print_board(3, 15));
+// console.log(print_line(10))
